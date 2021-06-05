b7ece78751f7a48b19066210f2d834339b5b3d9e
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-05 17:46:02 -0300

    agregamos url personalizada a las tablas

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,4 +1,5 @@
 const Proyectos = require('../models/Proyectos')
+const slug = require('slug');
 
 exports.proyectosHome = (req, res) => {
     res.render('index', {
@@ -32,7 +33,8 @@ exports.nuevoProyecto = async (req, res) =>{
     } else {
         //no hay errores 
         //Insertar en la BD
-        const proyecto = await Proyectos.create({ nombre });
+        const url = slug(nombre).toLowerCase()
+        const proyecto = await Proyectos.create({ nombre, url });
         res.redirect('/');      
     }
 }
