382df4ccc07bfbb0ea853731a5729f5e1485412f
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-05 19:50:38 -0300

    insertamos url unica con Shortid

--- models/Proyectos.js
+++ models/Proyectos.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 const sequelize = require('sequelize');
 
-const slug = require('slug');
-
 const db = require('../config/db');
+const slug = require('slug');
+const shortid = require('shortid');
 
 const Proyectos = db.define('proyectos', {
     id : {
@@ -20,7 +20,7 @@ const Proyectos = db.define('proyectos', {
         beforeCreate(proyecto){
             const url = slug(proyecto.nombre).toLowerCase();
 
-            proyecto.url = url;
+            proyecto.url = `${url}-${shortid.generate()}`
         }
     }
 });
