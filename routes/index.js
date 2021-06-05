d46c057c77c33262c070f25d2ac390440a00937e
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-05 17:27:49 -0300

    sacamos /check de express-validator

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 //importar express validator
-const { body } = require('express-validator/check')
-
+//const { body } = require('express-validator/check')
+const { body } = require('express-validator')
 //importar el controlador 
 const proyectosController = require
 ('../controllers/proyectosController');
