754c64cfb543a250f2b9892463484ab5ffb30aa4
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-07 20:44:18 -0300

    colocamos los proyectos en visibles en el aside

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -9,12 +9,15 @@ exports.proyectosHome = async(req, res) => {
         proyectos
     });
 }
-exports.formularioProyecto = (req, res) =>{
+exports.formularioProyecto = async (req, res) =>{
+    const proyectos = await Proyectos.findAll();
     res.render('nuevoProyecto', {
-        nombrePagina: 'Nuevo Proyecto'
+        nombrePagina: 'Nuevo Proyecto',
+        proyectos
     });
 }
 exports.nuevoProyecto = async (req, res) =>{
+    const proyectos = await Proyectos.findAll();
     //enviar a la consola lo que el usuiario escriba
     //console.log(req.body);
 
@@ -31,7 +34,8 @@ exports.nuevoProyecto = async (req, res) =>{
     if(errores.length > 0) {
         res.render('nuevoProyecto', {
             nombrePagina : 'Nuevo Proyecto',
-            errores
+            errores,
+            proyectos
         })
     } else {
         //no hay errores 
@@ -42,6 +46,21 @@ exports.nuevoProyecto = async (req, res) =>{
     }
 }
 
-exports.proyectoPorUrl = (req, res) => {
-    res.send('Listo');
+exports.proyectoPorUrl = async (req, res, next) => {
+    const proyectos = await Proyectos.findAll();
+    
+    const proyecto = await Proyectos.findOne({
+        where: {
+            url: req.params.url
+        }
+    });
+
+    if(!proyecto) return next();
+
+    //render a la vista
+    res.render('tareas', {
+        nombrePagina : 'Tareas del Proyecto',
+        proyecto,
+        proyectos
+    });
 }
