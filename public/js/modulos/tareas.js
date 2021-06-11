1bf7ee88f9672752281b9f0249d61373b9e274b4
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-11 02:17:07 -0300

    marcamos tareas que ya han sido completadas

--- public/js/modulos/tareas.js
+++ public/js/modulos/tareas.js
@@ -14,7 +14,9 @@ if(tareas){
             const url = `${location.origin}/tareas/${idTarea}`;
             axios.patch(url, { idTarea })
             .then(function(respuesta){
-                console.log(respuesta)
+                if(respuesta.status === 200){
+                    icono.classList.toggle('completo')
+                }
             })
         }
     })
