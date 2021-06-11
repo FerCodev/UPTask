abad6fb7629b41bdc7b0fa18dc86f5c8e9f45b50
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-11 19:56:20 -0300

    hasheando las paswords sin hasMany(proyectos)

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../config/db')
 const Proyectos = require('../models/Proyectos')
+const bcrypt = require('bcrypt-nodejs')
 
 
 const Usuarios = db.define('usuarios', {
@@ -17,8 +18,15 @@ const Usuarios = db.define('usuarios', {
         type: Sequelize.STRING(60),
         allowNull: false
     }
+},{
+    hooks: {
+        beforeCreate(usuario){
+            usuario.password = bcrypt.hashSync(usuario.password, bcrypt.genSaltSync(10));
+        }
+    }
 });
-Usuarios.hasMany(Proyectos)
+
+//Usuarios.hasMany(Proyectos)
 
 module.exports = Usuarios;
 
