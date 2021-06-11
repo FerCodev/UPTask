7f0bc54a7c82acff9ca56a693e96cac6dde43222
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-11 17:40:05 -0300

    Creacion del modelo de Usuarios

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const db = require('./config/db');
 //Importar el modelo
 require('./models/Proyectos');
 require('./models/Tareas');
+require('./models/Usuarios');
 
 db.sync()
     .then(() => console.log('conectado al servidor'))
