c7cb4533da3d78ab2f409500fe88a4878b96960d
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-12 01:52:11 -0300

    validando formulario para crear cuentas (try/catch)en usuariosController

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const routes = require('./routes');
 const path = require('path');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+const flash = require('connect-flash')
 
 //importamos helpers de forma global 
 const helpers = require('./helpers')
@@ -31,6 +32,9 @@ db.sync()
 //crear una app de express
 const app = express();
 
+//habilitar bodyParser para leer datos del formulario   
+app.use(bodyParser.urlencoded({extended: true}));
+
 //agregamos express validator a toda la aplicacion
 app.use(expressValidator());
 
@@ -44,25 +48,15 @@ app.set('view engine', 'pug');
 //agregar la carpeta de las vistas
 app.set('views', path.join(__dirname, './views'));
 
+//agregar flash messages
+app.use(flash())
+
 // pasar vardump a la app 
 app.use((req, res, next) => {
     res.locals.vardump = helpers.vardump;
     next();
 });
 
-//Aprendiendo middleware
-//app.use((req, res,next)=>{
-    //console.log('yo soy middleware');
-    //next();
-//});
-//app.use((req, res, next)=>{
-    //console.log('yo soy otre middleware');
-    //next();
-//});
-
-
-//habilitar bodyParser para leer datos del formulario   
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', routes());
 
