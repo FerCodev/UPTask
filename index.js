b304c7850b48eb241cf70852401003c12c4ae8bc
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-12 02:29:26 -0300

    agregando conect fash para obtener mayor flexibilidad con las alertas

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const flash = require('connect-flash')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 
 //importamos helpers de forma global 
 const helpers = require('./helpers')
@@ -32,18 +34,17 @@ db.sync()
 //crear una app de express
 const app = express();
 
+//donde cargar los archivos estaticos
+app.use(express.static('public'));
+
+//habilitar pug
+app.set('view engine', 'pug');
+
 //habilitar bodyParser para leer datos del formulario   
 app.use(bodyParser.urlencoded({extended: true}));
 
 //agregamos express validator a toda la aplicacion
 app.use(expressValidator());
-
-//donde cargar los archivos estaticos
-app.use(express.static('public'));
-
-
-//habilitar pug
-app.set('view engine', 'pug');
 
 //agregar la carpeta de las vistas
 app.set('views', path.join(__dirname, './views'));
@@ -51,9 +52,20 @@ app.set('views', path.join(__dirname, './views'));
 //agregar flash messages
 app.use(flash())
 
+app.use(cookieParser())
+
+//las sesiones nos permiten navegar por
+//distintas paginas sin tener que volvernos a autenticar
+app.use(session({
+    secret: 'supersecreto',
+    resave: false,
+    saveUninitialized: false
+}))
+
 // pasar vardump a la app 
 app.use((req, res, next) => {
     res.locals.vardump = helpers.vardump;
+    res.locals.mensajes = req.flash()
     next();
 });
 
