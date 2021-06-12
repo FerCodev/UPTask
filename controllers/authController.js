e1b448d67047cec5fd1dd021f5ab3d67305b211a
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-12 20:22:12 -0300

    verificando que el token sea valido

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,38 +29,72 @@ exports.cerrarSesion = (req, res) => {
     })
 }
 
-//genera un token si el usuario es valido 
+// genera un token si el usuario es valido
 exports.enviarToken = async (req, res) => {
-    //verificar que un usuario existe
-    const usuario = await Usuarios.findOne({
-        where: {
-            email: req.body.email
-        }
-    }) 
-    //Si no hay usuario 
-    if(!usuario){
-        req.flash('error', 'No existe esa cuenta')
-        res.redirect('reestablecer');
-        /* res.render('reestablecer', {
-            nombrePagina: 'Reestablecer tu Password',
-            mensajes: req.flash()
-        }) */
+    // verificar que el usuario existe
+    const {email} = req.body
+    const usuario = await Usuarios.findOne({where: { email }});
+
+    // Si no existe el usuario
+    if(!usuario) {
+        req.flash('error', 'No existe esa cuenta');
+        res.redirect('/reestablecer');
     }
-    // Usuario existe 
-    usuario.token = crypto.randomBytes(20).toString('hex')
-    
-    //expiracion
-    usuario.expiracion = Date.now() + 3600000
-    
-    // Guardarlos en la base de datos
+
+    // usuario existe
+    usuario.token = crypto.randomBytes(20).toString('hex');
+    usuario.expiracion = Date.now() + 3600000;
+
+    // guardarlos en la base de datos
     await usuario.save();
 
-    //url de reset
-    //const reqq = req.headers.host
-    //const usertToken = usuario.token
-    const resetUrl = `http://${req.headers.host}/reestablecer/${usuario.token}`
-    console.log(resetUrl)
+    // url de reset
+    const resetUrl = `http://${req.headers.host}/reestablecer/${usuario.token}`;
+//console.log(usuario.token)
+    // Enviar el Correo con el Token
+
+    //await enviarEmail.enviar({
+        //usuario,
+        //subject: 'Password Reset', 
+        //resetUrl, 
+        //archivo : 'reestablecer-password'
+    //});
+
+    // terminar
+    //req.flash('correcto', 'Se envió un mensaje a tu correo');
+    //res.redirect('/iniciar-sesion');
 }
-exports.resetPassword = async (req, res) =>{
-    res.json(req.params.token)
+exports.resetPassword = async (req, res) => {
+    const usuario = await Usuarios.findOne({
+        where: {
+            token: req.params.token
+        }
+    });
+
+    // sino encuentra el usuario
+    if(!usuario) {
+        req.flash('error', 'No Válido');
+        res.redirect('/reestablecer');
+    }
+
+    // Formulario para generar el password
+    res.render('resetPassword', {
+        nombrePagina : 'Reestablecer Contraseña'
+    })
 }
+
+
+/* exports.resetPassword = async (req, res) =>{
+    const usuario = await Usuarios.findOne({
+        where: {
+            token: req.param.token
+        }
+    });
+
+    // Si no encuentre el usuario   
+    if(!usuario){
+        req.flash('error', 'No Valido');
+        res.redirect('/reestablecer')
+    }
+    console.log(usuario)
+} */
